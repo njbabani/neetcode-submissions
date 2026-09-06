01b954cae6e2232d884e91894503c9495f9a0158
Nikhil J. Babani <147331880+njbabani@users.noreply.github.com>
2026-09-06 14:56:25 -0400

    Create 155-MinStack

---
+++ leetcode/155-MinStack/155-MinStack.py
@@ -0,0 +1,34 @@
+# Last updated: 9/6/2026, 2:56:22 PM
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, value: int) -> None:
+        self.stack.append(value)
+
+        if not self.min_stack:
+            self.min_stack.append(value)
+        else:
+            min_value = min(self.min_stack[-1], value)
+            self.min_stack.append(min_value)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(value)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
