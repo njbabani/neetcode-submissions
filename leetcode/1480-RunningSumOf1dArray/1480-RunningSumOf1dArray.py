df46fde335e9d474bd0e21e418a7b5bca7cf50bf
Nikhil J. Babani <147331880+njbabani@users.noreply.github.com>
2026-09-06 14:55:45 -0400

    Create 1480-RunningSumOf1dArray

---
+++ leetcode/1480-RunningSumOf1dArray/1480-RunningSumOf1dArray.py
@@ -0,0 +1,6 @@
+# Last updated: 9/6/2026, 2:55:42 PM
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1]
+        return nums
